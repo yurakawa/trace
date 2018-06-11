ee383584edb49400a79340e4b2146417817f31f1
y.urakawa <s08a2015@gmail.com>
2018-06-11 13:38:54 +0900

    Refactoring

--- tracer.go
+++ tracer.go
@@ -5,39 +5,22 @@ import (
 	"fmt"
 )
 
-// Tracer is the interface that describes an object capable of
-// tracing events throughout code.
-type Tracer interface {
-	Trace(...interface{})
-}
-
-// tracer is a Tracer that writes to an
-// io.Writer.
-type tracer struct {
+// tracer is a tracing events throughout code.
+type Tracer struct {
 	out io.Writer
 }
 
 // Trace writes the arguments to this Tracers io.Writer.
-func (t *tracer) Trace(a ...interface{}) {
-	t.out.Write([]byte(fmt.Sprint(a...)))
-	t.out.Write([]byte("\n"))
+func (t *Tracer) Trace(a ...interface{}) {
+	if t == nil  || t.out == nil {
+		return
+	}
+	fmt.Fprintln(t.out, a...)
 }
 
 // New creates a new Tracer that will write the output to
 // the specified io.Writer.
-func New(w io.Writer) Tracer {
-	return &tracer{out: w}
-}
-
-
-// nilTracer
-type nilTracer struct {}
-
-// Trace for a nil tracer does nothing.
-func (t *nilTracer) Trace(a ...interface{}) {}
-
-// Off creates a Tracer that will ignore calls to Trace.
-func Off() Tracer {
-	return &nilTracer{}
+func New(w io.Writer) *Tracer {
+	return &Tracer{out: w}
 }
 
--- tracer_test.go
+++ tracer_test.go
@@ -9,17 +9,13 @@ import (
 func TestNew(t *testing.T) {
 	var buf bytes.Buffer
 	tracer := New(&buf)
-	if tracer == nil {
-		t.Error("Return from New should not be nil")
-	} else {
-		tracer.Trace("Hello trace package.")
-		if buf.String() !=  "Hello trace package.\n" {
-			t.Error("Trace should not write '%s'.", buf.String())
-		}
+	tracer.Trace("Hello trace package.")
+	if buf.String() !=  "Hello trace package.\n" {
+		t.Errorf("Trace should not write '%s'.", buf.String())
 	}
 }
 
 func TestOff(t *testing.T) {
-	var silentTracer Tracer = Off()
-	silentTracer.Trace("something")
+	var silentTracer *Tracer
+	silentTracer.Trace("Data")
 }
